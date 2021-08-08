0bfa6ee946c206cef6a8923fd3a97244a419f124
Hoc Nguyen <hoc.nguyen@meganet.com.vn>
2021-08-08 14:09:06 +0700

    Finish

--- js/mainJs.js
+++ js/mainJs.js
@@ -21,10 +21,33 @@ $(function() {
 	   	},
     	minDate: 0,
     	maxDate: "+1M +10D",
+    	showOn: "both", 
+      	buttonImage: "../assets/icon/chevron-compact-down.png",
+      	buttonImageOnly: true,
+      	// buttonText: '<!-- <i class="bi bi-chevron-compact-down text-white"></i> -->'
     });
  });
+
+$(".ui-datepicker-trigger").click(function() {
+	$([document.documentElement, document.body]).animate({
+        scrollTop: $("#header_title").offset().top
+    }, 500);
+})
 
 // $(".retreat").hover(function() {
 // 	$(this).find('.retreat_content_title').addClass('fade-in');
 // 	$(this).find('.retreat_content_text').addClass('fade-in');
 // })
+
+if (screen.width <= 578) {
+	$("body *").removeAttr('data-aos-delay');
+}
+
+function getParameterByName(name, url = window.location.href) {
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
